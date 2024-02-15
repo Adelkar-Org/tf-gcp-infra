4a6c665ac0a3397d639fd2101628d3e49acfe118
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-02-15 15:08:44 -0500

    removed var and added to new file

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-# provider "google" {
-#   project = var.project_id
-#   region  = var.region
-# }
-
 resource "google_compute_network" "vpc_network" {
-  name                    = "my-vpc"
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+  name                            = "my-vpc"
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
+  delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp" {
--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
-  default     = "csye6225-cloud"
 }
 
 variable "region" {
   description = "The region where resources will be created"
   type        = string
-  default     = "us-east1"
 }

c4ba71ada7c8da6ae0b48cabbffe0dfb8a02a66f
Mihir Adelkar <adelkar.m@northeastern.edu>
2024-04-09 16:13:21 -0400

    Update service-account.tf

--- service-account.tf
+++ service-account.tf
@@ -3,10 +3,10 @@ resource "google_service_account" "gcf_sa" {
   display_name = var.gcf_sa_display_name
 }
 
-resource "google_service_account" "logging_account" {
-  account_id   = var.logging_account_name
-  display_name = var.logging_account_display_name
-}
+# resource "google_service_account" "logging_account" {
+#   account_id   = var.logging_account_name
+#   display_name = var.logging_account_display_name
+# }
 
 resource "google_service_account" "vm_instance_account" {
   account_id   = var.vm_instance_account_name
@@ -23,7 +23,7 @@ resource "google_project_iam_binding" "logging_admin" {
   project = var.project_id
   role    = var.logging_admin_role
   members = [
-    "serviceAccount:${google_service_account.logging_account.email}",
+    "serviceAccount:${google_service_account.vm_instance_account.email}",
   ]
 }
 
@@ -31,7 +31,7 @@ resource "google_project_iam_binding" "monitoring_metric_writer" {
   project = var.project_id
   role    = var.monitoring_metric_writer_role
   members = [
-    "serviceAccount:${google_service_account.logging_account.email}",
+    "serviceAccount:${google_service_account.vm_instance_account.email}",
   ]
 }
 
@@ -39,6 +39,6 @@ resource "google_pubsub_topic_iam_binding" "publisher" {
   topic = google_pubsub_topic.verify_email.id
   role  = var.pubsub_publisher_role
   members = [
-    "serviceAccount:${google_service_account.logging_account.email}",
+    "serviceAccount:${google_service_account.vm_instance_account.email}",
   ]
 }
